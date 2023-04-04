4198e2786d75732afa664dadbfca698039543a1d
Zueon <maineon0701@gmail.com>
2023-04-04 14:44:10 +0900

    이메일 전송 기능 구현 (HTML 페이지 보내기)

--- src/main/java/com/zueon/springbootapp/account/application/AccountService.java
+++ src/main/java/com/zueon/springbootapp/account/application/AccountService.java
@@ -5,6 +5,7 @@ import com.zueon.springbootapp.account.domain.entity.Account;
 import com.zueon.springbootapp.account.domain.entity.Zone;
 import com.zueon.springbootapp.account.endpoint.controller.SignUpForm;
 import com.zueon.springbootapp.account.infra.AccountRepository;
+import com.zueon.springbootapp.config.AppProperties;
 import com.zueon.springbootapp.mail.EmailMessage;
 import com.zueon.springbootapp.mail.service.EmailService;
 import com.zueon.springbootapp.settings.controller.NotificationForm;
@@ -12,6 +13,7 @@ import com.zueon.springbootapp.settings.controller.Profile;
 import com.zueon.springbootapp.tag.domain.entity.Tag;
 import com.zueon.springbootapp.zone.infra.ZoneRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +23,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.Collections;
 import java.util.Optional;
@@ -29,11 +33,14 @@ import java.util.Set;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Log4j2
 public class AccountService implements UserDetailsService {
     private final AccountRepository accountRepository;
     private final ZoneRepository zoneRepository;
     private final EmailService emailService;
     private final PasswordEncoder passwordEncoder;
+    private final TemplateEngine templateEngine;
+    private final AppProperties appProperties;
 
 
     public Account signUp(SignUpForm signUpForm) {
@@ -54,11 +61,21 @@ public class AccountService implements UserDetailsService {
     }
 
     public void sendVerificationEmail(Account newAccount){
+        Context context = new Context();
+        context.setVariable("link", String.format("/check-email-token?token=%s&email=%s"
+                ,newAccount.getEmailToken(), newAccount.getEmail()));
+        context.setVariable("nickname", newAccount.getNickname());
+        context.setVariable("linkName", "이메일 인증하기");
+        context.setVariable("message","StudyApp 가입 인증을 위해 링크를 클릭하세요");
+        context.setVariable("host", appProperties.getHost());
+
+        String message = templateEngine.process("mail/simple-link", context);
+
         emailService.sendEmail(
                 EmailMessage.builder()
                         .to(newAccount.getEmail())
                         .subject("StudyApp 회원 가입 인증")
-                        .message(String.format("/check-email-token?token=%s&email=%s", newAccount.getEmailToken(), newAccount.getEmail()))
+                        .message(message)
                         .build()
 
         );
@@ -121,12 +138,21 @@ public class AccountService implements UserDetailsService {
     }
 
     public void sendLoginLink(Account account){
+        Context context = new Context();
+        context.setVariable("link", "/login-by-email?token=" + account.getEmailToken() + "&email=" + account.getEmail());
+        context.setVariable("nickname", account.getNickname());
+        context.setVariable("linkName", "StudyApp 로그인하기");
+        context.setVariable("message","로그인하려면 아래 링크를 클릭하세요");
+        context.setVariable("host", appProperties.getHost());
+
+        String message = templateEngine.process("mail/simple-link", context);
+
         account.generateToken();
         emailService.sendEmail(
                 EmailMessage.builder()
                         .to(account.getEmail())
                         .subject("[StudyApp] 로그인 링크")
-                        .message("/login-by-email?token=" + account.getEmailToken() + "&email=" + account.getEmail())
+                        .message(message)
                         .build()
         );
     }
@@ -164,4 +190,6 @@ public class AccountService implements UserDetailsService {
                 .ifPresent(a -> a.getZones().remove(zone));
 
     }
+
+
 }
